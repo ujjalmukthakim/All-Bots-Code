ebb039caeb064ec7e509703c5bbbd9ef1e306d76
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-08-07 12:54:37 +0600

    full stack folder create system

--- LaunchPad.py
+++ LaunchPad.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 print('========================\n' \
 'Developer LaunchPad\n' \
@@ -9,8 +10,33 @@ print('========================\n' \
 '4. Python Bot\n' \
 '5. Quit')
 
-choice_input=int(input('- '))
 
-if choice_input==3:
-    project_name=input("Project Name: ")
+folders=[
+    'backend',
+    'frontend',
+    'docs',
+    'docker',
+    'scripts'
+]
 
+files=[
+    'README.md',
+    '.gitignore',
+    '.env.example'
+]
+
+
+try: 
+   choice_input=int(input('- '))
+   if choice_input==3:
+       project_name=input("Project Name: ")
+
+   folder_path=Path(f'/Users/apple/desktop/{project_name}')
+   folder_path.mkdir(parents=True,exist_ok=True)
+   for folder in folders:
+       (folder_path / folder).mkdir(parents=True,exist_ok=True)
+   for file in files:
+       (folder_path / file).touch()
+       
+except:
+    print('Error, This File Only Accept Number !')
